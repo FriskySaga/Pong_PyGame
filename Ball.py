4b22d37e1ed5bb7e4e654a926109bde466507801
FriskySaga <28899815+FriskySaga@users.noreply.github.com>
2021-09-06 17:20:49 -0400

    Remove unused code

--- Ball.py
+++ Ball.py
@@ -1,20 +1,12 @@
 from random import randint
 import pygame
 
-import Colors
-
 class Ball(pygame.sprite.Sprite):
   def __init__(self, color, width, height):
     super().__init__()
-
     self.image = pygame.Surface([width, height])
-    self.image.fill(Colors.BLACK)
-    self.image.set_colorkey(Colors.BLACK)
-
     pygame.draw.rect(self.image, color, [0, 0, width, height])
-
     self.velocity = [randint(4,8), randint(-8,-8)]
-
     self.rect = self.image.get_rect()
   
   def update(self):
